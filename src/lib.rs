fb513b724b8963d714ec0f9209e89605608a7bc6
Ross Tollefson <ross@past9systems.com>
2020-05-12 02:10:55 -0600

    Add module docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,28 @@
+//! Expands arrays and resolves inheritance chains in CMSIS-SVD specifications. 
+//! 
+//! ## Example usage:
+//! 
+//! ```rust
+//! use std::fs::File;
+//! use std::io::Read;
+//! use svd_expander::DeviceSpec;
+//! 
+//! fn main() {
+//!     let xml = &mut String::new();
+//! 
+//!     File::open("./stm32f303.svd")
+//!         .unwrap()
+//!         .read_to_string(xml)
+//!         .unwrap();
+//! 
+//!     println!("{:#?}", DeviceSpec::from_xml(xml).unwrap());
+//! }
+//! ```
+//! 
+//! This crate is intended for use in code generators. It is under active development and bug reports are welcome. 
+//! 
+//! Feature requests may be considered, but [svd-expander](https://crates.io/crates/svd-expander) depends on [svd-parser](https://crates.io/crates/svd-parser) (at least for now) to parse the SVD files, so it can only implement the features supported by the parser.
+
 use svd_parser::Access;
 
 mod cluster;
